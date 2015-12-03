ca314e8c05225392927540063e25be86fef0e099
Ron Daryl L. Magno <ron.zohan@yahoo.com>
2015-12-03 15:42:55 +0800

    Fixed spacing

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
 from flask_restful import Api
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from resource.location import Location
 
 app = Flask(__name__)
 api = Api(app)
 
-
 api.add_resource(Location, '/api/location')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
