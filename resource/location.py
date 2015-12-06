98b262a07c98624357dc492c1e1485faefac12bb
Ron Daryl L. Magno <ron.zohan@yahoo.com>
2015-12-06 22:32:08 +0800

    Added filter on getting upload time between two dates

--- resource/location.py
+++ resource/location.py
@@ -1,12 +1,10 @@
+import datetime
 from flask_restful import Resource, reqparse
 from models.shared_model import db
 from models.user import User
 
 
 class Location(Resource):
-    def dispatch_request(self):
-        pass
-
     def post(self):
         parser = reqparse.RequestParser()
         parser.add_argument('longitude', required=True)
@@ -15,7 +13,7 @@ class Location(Resource):
 
         args = parser.parse_args()
 
-        user = User(args['longitude'],  args['latitude'], args['upload_time'])
+        user = User(args['longitude'], args['latitude'], args['upload_time'])
         db.session.add(user)
         db.session.commit()
 
@@ -25,8 +23,17 @@ class Location(Resource):
         parser = reqparse.RequestParser()
         parser.add_argument('limit', required=False)
         parser.add_argument('upload_time_order', required=False, default="asc")
+        parser.add_argument('filter_start_date', required=False, default=datetime.date.today())
+        parser.add_argument('filter_end_date', required=False, default=datetime.date.today())
 
         args = parser.parse_args()
+
+        # if start date and end date are the same, add one day to the end date to
+        # have the query date >= TODAY and date <= TOMORROW to get all the queries
+        # of from time of 00:00:00 up to 23:59:59
+        if args['filter_start_date'] == args['filter_end_date']:
+            args['filter_end_date'] = datetime.datetime.strptime(args['filter_end_date'], "%Y-%m-%d") \
+                                      + datetime.timedelta(days=1)
 
         # This will check what will be the ordering of the returned location info
         if args['upload_time_order'] == "asc":
@@ -37,9 +44,12 @@ class Location(Resource):
             upload_time_order = User.upload_time.asc()
 
         if args['limit'] is not None:
-            users = db.session.query(User).limit(args['limit']).order_by(upload_time_order)
+            users = db.session.query(User) \
+                .filter(User.upload_time.between(args['filter_start_date'], args['filter_end_date'])) \
+                .order_by(upload_time_order).limit(args['limit'])
         else:
-            users = db.session.query(User).order_by(User.upload_time.desc(upload_time_order)).all()
+            users = db.session.query(User).order_by(upload_time_order).all() \
+                .filter(User.upload_time.between(args['filter_start_date'], args['filter_end_date']))
 
         user_locations = []
 
@@ -50,4 +60,4 @@ class Location(Resource):
                 "upload_time": str(user.upload_time)
             })
 
-        return {"locations": user_locations}
+        return {"message": "Success", "locations": user_locations}
