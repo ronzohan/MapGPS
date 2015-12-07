77121d0e598ae8e866f058eb6fc0458a365fa04c
Ron Daryl L. Magno <ron.zohan@yahoo.com>
2015-12-07 23:22:04 +0800

    Added cors request

--- app.py
+++ app.py
@@ -13,6 +13,13 @@ def create_database():
      db.create_all()
      db.session.commit()
 
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    return response
+
 # load all config files
 app.config.from_pyfile('config.py')
 api.add_resource(Location, '/api/location')
